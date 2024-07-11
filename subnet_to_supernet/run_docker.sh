cd2e12c39144d814b9652033b0ca59e0454266f2
Sagar1005 <84617537+Sagar-1005@users.noreply.github.com>
2024-07-11 19:56:35 +0530

    Update run_docker.sh

--- subnet_to_supernet/run_docker.sh
+++ subnet_to_supernet/run_docker.sh
@@ -1,6 +1,13 @@
-#!bin/bash
+#!/bin/bash
+
+# Ensure the output file exists
 touch coalesce_output.csv
 
+# Build the Docker image
 docker build -t subnet_processor .
 
-docker run --rm -v "$(pwd)/coalesce_input.csv:/home/tufin/coalesce_input.csv" -v "$(pwd)/coalesce_output.csv:/home/tufin/coalesce_output.csv" subnet_processor /home/tufin/coalesce_input.csv /home/tufin/coalesce_ouput.csv
+# Run the Docker container
+docker run --rm \
+  -v "$(pwd)/coalesce_input.csv:/home/tufin/coalesce_input.csv" \
+  -v "$(pwd)/coalesce_output.csv:/home/tufin/coalesce_output.csv" \
+  subnet_processor /home/tufin/coalesce_input.csv /home/tufin/coalesce_output.csv
