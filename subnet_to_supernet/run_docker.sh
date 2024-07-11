f677e358ba12009ff60fdeaf38fc00195c2bb66c
Sagar-1005 <admin@example.com>
2024-07-11 23:12:27 +0530

    added code

--- subnet_to_supernet/run_docker.sh
+++ subnet_to_supernet/run_docker.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Create empty output file to ensure file exists.
-touch coalesceOutput.csv
+touch coalesce_output.csv
 
 # Build the Docker image
 docker build -t subnet_processor .
 
 # Run the Docker container
 docker run --rm \
-  -v "$(pwd)/coalesceInput.csv:/home/tufin/coalesceInput.csv" \
-  -v "$(pwd)/coalesceOutput.csv:/home/tufin/coalesceOutput.csv" \
-  subnet_processor /home/tufin/coalesceInput.csv /home/tufin/coalesceOutput.csv
+  -v "$(pwd)/coalesce_input.csv:/home/tufin/coalesce_input.csv" \
+  -v "$(pwd)/coalesce_output.csv:/home/tufin/coalesce_output.csv" \
+  subnet_processor /home/tufin/coalesce_input.csv /home/tufin/coalesce_output.csv
